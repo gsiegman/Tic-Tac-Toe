c8522cc6dc5e29681daa6fa40512d735ec78e8a3
Glenn Siegman <gsiegman@gsiegman.com>
2010-10-06 23:45:13 -0400

    defined basic game grid structure

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,23 +2,28 @@ class Game(object):
     """
     A Game of Tic-Tac-Toe
     """
+    WINNING_COMBINATIONS = [
+    	[1, 2, 3], [4, 5, 6],
+    	[7, 8, 9], [1, 4, 7],
+    	[2, 5, 8], [3, 6, 9],
+    	[1, 5, 9], [3, 5, 7],
+    ]
     
     def __init__(self):
-        pass
-
+        self.board = [
+            '1', '2', '3',
+            '4', '5', '6',
+            '7', '8', '9'
+        ]
 
 def play():
     """
     Runs the game loop
     """
     game = Game()
-	
+    
     while 1:
         pass
 
 if __name__ == "__main__":
     play()	
-
-	
-	
-
