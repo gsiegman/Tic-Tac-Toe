b954f9984aa604973c200aeaf2984220579f9fb1
Glenn Siegman <gsiegman@gsiegman.com>
2010-10-06 21:38:13 -0400

    stubbed out game board and play event loop

---
+++ tic_tac_toe.py
@@ -0,0 +1,24 @@
+class Game(object):
+    """
+    A Game of Tic-Tac-Toe
+    """
+    
+    def __init__(self):
+        pass
+
+
+def play():
+    """
+    Runs the game loop
+    """
+    game = Game()
+	
+    while 1:
+        pass
+
+if __name__ == "__main__":
+    play()	
+
+	
+	
+
